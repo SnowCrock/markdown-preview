65ec5184dd042d548f168dc5d21d9b645f95c4b1
lizhangrui <ja131484@gmail.com>
2018-09-28 01:36:35 +0800

    md生成react时添加key 根据deep+index生成

--- loaders/markdown-to-react.js
+++ loaders/markdown-to-react.js
@@ -8,15 +8,15 @@ loadLanguages(['jsx', 'json'])
 module.exports = function (source) {
   source.shift()
   let reactApp = '[\n'
-  source.forEach(element => {
+  source.forEach((element, index) => {
     if (!element) return
-    reactApp += generateSource(element, 1)
+    reactApp += generateSource(element, 1, index)
   })
   reactApp += ']'
   return reactApp
 }
 
-function generateSource(source, deep) {
+function generateSource(source, deep, index) {
   deep += 1
   const nextDeep = deep + 1
   if (typeof source === 'string') {
@@ -24,33 +24,37 @@ function generateSource(source, deep) {
   }
 
   let [type, children] = source
+  // 为pre类型  代码类型
   if (type === 'pre') {
     const lang = getLang(source)
     const code = Prism.highlight(getCode(source), Prism.languages[lang], lang)
-    return createCodeElement(code, lang, deep)
+    return createCodeElement(code, lang, deep, index)
   }
   children = source.length > 2 ? source.slice(1) : children
+  // 判断children是否为数组
   if (Array.isArray(children)) {
+    // children是否为element
     if (isElement(children)) {
       const childType = children.shift()
       if (children.length === 1 && !Array.isArray(children)) {
         const child = children.shift()
-        return createElement(childType, child, deep)
+        return createElement(childType, child, deep, index)
       }
-      const childElement = createElement(childType, children.map(item => generateSource(item, nextDeep + 1)), deep + 1)
-      return createElement(type, '\n' + childElement, deep)
+      const childElement = createElement(childType, children.map((item, idx) => generateSource(item, nextDeep + 1, idx)), deep + 1, index)
+      return createElement(type, '\n' + childElement, deep, index)
     }
 
-    return createElement(type, children.map(item => generateSource(item, nextDeep)), deep)
+    return createElement(type, children.map((item, idx) => generateSource(item, nextDeep, idx)), deep, index)
   }
-  return createElement(type, children, deep)
+  return createElement(type, children, deep, index)
 }
 
-function createElement(type, children, deep) {
+function createElement(type, children, deep, index) {
+  const key = `${deep}+${index}`
   let strDeep = indent(deep)
   const childrenDeep = indent(deep - 2)
   if (!type) return `${strDeep}null,\n`
-  if (children === undefined) return `${strDeep}React.createElement('${type}'),\n`
+  if (children === undefined) return `${strDeep}React.createElement('${type}', { key: ${key} }),\n`
   if (typeof children !== 'string') {
     if (Array.isArray(children)) {
       children = '[\n' + toString(children) + strDeep + indent(1) + '],'
@@ -60,32 +64,40 @@ function createElement(type, children, deep) {
   } else {
     children = children
   }
+  // 没换行 且不等于tr
   if (!hasChangeLine(children) && type !== 'tr') {
-    return `${strDeep}React.createElement('${type}', {children: ${children}}),\n`
+    return `${strDeep}React.createElement('${type}', {key: ${key}, children: ${children}}),\n`
   } else if (type === 'tr') {
     // strDeep = '\n' + strDeep
+
+    // 如果是文本且不是字符串数组，将其变成字符串数组， 如果没执行说明是数组元素
   } else if (isChildText(children) && children.indexOf('[') < 0) {
     return (
       `${strDeep}React.createElement('${type}', {
+        key: ${key},
         children: [
           ${stringToArray(children)},
         ]}),\n`
     )
   }
+  
+  // 创建正常children， children是数组
   return (
     `${strDeep}React.createElement('${type}', {
+      ${childrenDeep}key: ${key},
       ${childrenDeep}children: ${children}
     ${childrenDeep}}),\n`
   )
 }
 
-function createCodeElement(code, lang, deep) {
+function createCodeElement(code, lang, deep, index) {
+  const key = `${deep}+${index}`
   const strDeep = indent(deep - 2)
   return (
     `${strDeep}React.createElement(
       ${strDeep}"pre",
-      ${strDeep}{ className: "language-${lang}" },
-      ${strDeep}React.createElement("code", { className: "language-${lang}", dangerouslySetInnerHTML: { __html: \`${code}\` } })
+      ${strDeep}{ key: ${key}, className: "language-${lang}" },
+      ${strDeep}React.createElement("code", { key: ${key}, className: "language-${lang}", dangerouslySetInnerHTML: { __html: \`${code}\` } })
     ),\n`
   )
 }
